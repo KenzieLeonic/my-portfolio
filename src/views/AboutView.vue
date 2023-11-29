a7f2eb1ab59b56a805bf627cea1b2986d74aaed2
KenzieLeonic <57589336+KenzieLeonic@users.noreply.github.com>
2023-11-29 22:22:07 +0700

    import router

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <p> about </p>
+    </div>
+</template>
+<script lang="ts">
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/views/BlogView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        blog
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    
+}
+</script>
+
+<style lang="">
+    
+</style>
